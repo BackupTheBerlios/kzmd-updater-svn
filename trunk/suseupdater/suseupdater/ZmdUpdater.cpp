784f5a3b6ac261fb9c1a4cc8bbb06d6889089916
narayannewton <narayannewton@c26c3214-6115-0410-a013-f97626c16cce>
2006-08-21 03:21:15 +0000

    Only install patches marked as "Broken," which apparently means it is needed.

--- trunk/suseupdater/suseupdater/ZmdUpdater.cpp
+++ trunk/suseupdater/suseupdater/ZmdUpdater.cpp
@@ -313,7 +313,7 @@ void ZmdUpdater::gotPatchListing(QValueList<Patch> patchList) {
 	}
 
 	for (iter = patchList.begin(); iter != patchList.end(); iter++) {
-		if ((*iter).status == 2)
+		if ((*iter).status == 3)
 			continue;
 
 		newItem = new UpdateListItem(tempList, (*iter).description, QCheckListItem::CheckBox);
