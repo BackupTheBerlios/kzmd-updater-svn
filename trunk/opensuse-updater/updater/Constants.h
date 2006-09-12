e216770a3c2d66f1837a67469e89551fc266e077
narayannewton <narayannewton@c26c3214-6115-0410-a013-f97626c16cce>
2006-09-12 21:16:05 +0000

    Rename the images

--- trunk/opensuse-updater/updater/Constants.h
+++ trunk/opensuse-updater/updater/Constants.h
@@ -20,8 +20,8 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-#define PROGRAM_NAME "suseupdater"
-#define VERSION "0.1"
+#define PROGRAM_NAME "opensuse-updater"
+#define VERSION "0.2"
 
 #define TRAY_ICON_GREEN "suse_green.png"
 #define TRAY_ICON_RED	"suse_red.png"
